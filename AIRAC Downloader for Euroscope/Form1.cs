45d29aadb1d03ceaedca1f4b8efdf89307579a84
markolonier <markolonier@gmail.com>
2023-09-13 18:56:55 +0200

    Projektdateien hinzufügen.

---
+++ AIRAC Downloader for Euroscope/Form1.cs
@@ -0,0 +1,10 @@
+namespace AIRAC_Downloader_for_Euroscope
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
