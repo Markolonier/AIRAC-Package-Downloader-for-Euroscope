478db006ff2d5210259977e7c851e39ff7c119f7
Fabian Mark <fabianmark61@gmail.com>
2025-10-23 23:42:43 +0200

    Now Injecting PTT Keys into .prf Files

--- AIRAC Downloader for Euroscope/Code/Core/Injector.cs
+++ AIRAC Downloader for Euroscope/Code/Core/Injector.cs
@@ -65,6 +65,14 @@ namespace AIRAC_Downloader.Code.Core
             {
                 prf_inject_string += "TeamSpeakVccs\tTs3NickName\t" + Main_Form.nickname_tb.Text + "\n";
             }
+            if (Main_Form.g2a_ptt_cb.Checked)
+            {
+                prf_inject_string += "TeamSpeakVccs\tTs3G2APtt\t" + Datahandling.G2A_ScanCode + "\n";
+            }
+            if (Main_Form.g2g_ptt_cb.Checked)
+            {
+                prf_inject_string += "TeamSpeakVccs\tTs3G2GPtt\t" + Datahandling.G2G_ScanCode + "\n";
+            }
             if (Main_Form.playback_mode_cb.Checked)
             {
                 prf_inject_string += "TeamSpeakVccs\tPlaybackMode\t" + Main_Form.playback_mode_dd.Text + "\n";
