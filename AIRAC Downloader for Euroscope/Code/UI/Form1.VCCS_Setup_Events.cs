8d7c7324085b867960045eb049cf966d7eb68dfd
Fabian Mark <fabianmark61@gmail.com>
2026-04-25 00:24:22 +0200

    Changed all Methods to private
    Removed deprecated and unused stuff from Keyboard Listener

--- AIRAC Downloader for Euroscope/Code/UI/Form1.VCCS_Setup_Events.cs
+++ AIRAC Downloader for Euroscope/Code/UI/Form1.VCCS_Setup_Events.cs
@@ -15,7 +15,7 @@ namespace AIRAC_Downloader_for_Euroscope.Code.UI
         private VCCS_Keyboard_Listener.KeyResult G2Abttn= new();
         private VCCS_Keyboard_Listener.KeyResult G2Gbttn = new();
 
-        public void Nickname_cb_CheckedChanged(object sender, EventArgs e)
+        private void Nickname_cb_CheckedChanged(object sender, EventArgs e)
         {
             if (nickname_cb.Checked)
             {
@@ -27,7 +27,7 @@ namespace AIRAC_Downloader_for_Euroscope.Code.UI
             }
         }
 
-        public void Capture_mode_cb_CheckedChanged(object sender, EventArgs e)
+        private void Capture_mode_cb_CheckedChanged(object sender, EventArgs e)
         {
             if (capture_mode_cb.Checked == false)
             {
@@ -41,7 +41,7 @@ namespace AIRAC_Downloader_for_Euroscope.Code.UI
             }
         }
 
-        public void Capture_device_cb_CheckedChanged(object sender, EventArgs e)
+        private void Capture_device_cb_CheckedChanged(object sender, EventArgs e)
         {
             if (capture_device_cb.Checked == false)
             {
@@ -61,7 +61,7 @@ namespace AIRAC_Downloader_for_Euroscope.Code.UI
             }
         }
 
-        public void Playback_device_cb_CheckedChanged(object sender, EventArgs e)
+        private void Playback_device_cb_CheckedChanged(object sender, EventArgs e)
         {
             if (playback_device_cb.Checked == false)
             {
@@ -81,7 +81,7 @@ namespace AIRAC_Downloader_for_Euroscope.Code.UI
             }
         }
 
-        public void Playback_mode_cb_CheckedChanged(object sender, EventArgs e)
+        private void Playback_mode_cb_CheckedChanged(object sender, EventArgs e)
         {
             if (playback_mode_cb.Checked == false)
             {
@@ -95,7 +95,7 @@ namespace AIRAC_Downloader_for_Euroscope.Code.UI
             }
         }
 
-        public void G2a_ptt_cb_CheckedChanged(object sender, EventArgs e)
+        private void G2a_ptt_cb_CheckedChanged(object sender, EventArgs e)
         {
             if (g2a_ptt_cb.Checked == false)
             {
@@ -107,7 +107,7 @@ namespace AIRAC_Downloader_for_Euroscope.Code.UI
             }
         }
 
-        public async void G2a_btn_Click(object sender, EventArgs e)
+        private async void G2a_btn_Click(object sender, EventArgs e)
         {
             g2a_btn.Text = "Listening to Keyboard Input";
             var possibleScan = await VCCS_Keyboard_Listener.Instance.ListenAsync();
@@ -129,7 +129,7 @@ namespace AIRAC_Downloader_for_Euroscope.Code.UI
             }
         }
 
-        public void G2g_ptt_cb_CheckedChanged(object sender, EventArgs e)
+        private void G2g_ptt_cb_CheckedChanged(object sender, EventArgs e)
         {
             if (g2g_ptt_cb.Checked == false)
             {
@@ -141,7 +141,7 @@ namespace AIRAC_Downloader_for_Euroscope.Code.UI
             }
         }
 
-        public async void G2g_btn_Click(object sender, EventArgs e)
+        private async void G2g_btn_Click(object sender, EventArgs e)
         {
             g2g_btn.Text = "Listening to Keyboard Input";
             var possibleScan = await VCCS_Keyboard_Listener.Instance.ListenAsync();
@@ -162,116 +162,5 @@ namespace AIRAC_Downloader_for_Euroscope.Code.UI
                 }
             }
         }
-
-
-
-        // ------------------------------------------------------------
-        // Registrierung von Raw Input (Keyboard)
-        // ------------------------------------------------------------
-        private void RegisterForRawInput()
-        {
-            RAWINPUTDEVICE[] rid = new RAWINPUTDEVICE[1];
-            rid[0].usUsagePage = 0x01;   // Generic desktop controls
-            rid[0].usUsage = 0x06;       // Keyboard
-            rid[0].dwFlags = 0;          // Receive input even if not focused
-            rid[0].hwndTarget = this.Handle;
-
-            if (!RegisterRawInputDevices(rid, 1, (uint)Marshal.SizeOf(typeof(RAWINPUTDEVICE))))
-                MessageBox.Show("Raw Input Registrierung fehlgeschlagen.");
-            else
-                MessageBox.Show("Raw Input aktiviert. Drücke jetzt Tasten!");
-        }
-
-        // ------------------------------------------------------------
-        // Window-Message-Verarbeitung (WM_INPUT)
-        // ------------------------------------------------------------
-        protected override void WndProc(ref Message m)
-        {
-            const int WM_INPUT = 0x00FF;
-            if (m.Msg == WM_INPUT)
-            {
-                uint dwSize = 0;
-                GetRawInputData(m.LParam, RID_INPUT, IntPtr.Zero, ref dwSize, (uint)Marshal.SizeOf<RAWINPUTHEADER>());
-
-                IntPtr buffer = Marshal.AllocHGlobal((int)dwSize);
-                try
-                {
-                    if (GetRawInputData(m.LParam, RID_INPUT, buffer, ref dwSize, (uint)Marshal.SizeOf<RAWINPUTHEADER>()) == dwSize)
-                    {
-                        RAWINPUT raw = Marshal.PtrToStructure<RAWINPUT>(buffer);
-
-                        if (raw.header.dwType == RIM_TYPEKEYBOARD)
-                        {
-                            int scanCode = raw.data.MakeCode;
-                            bool isE0 = (raw.data.Flags & RI_KEY_E0) != 0;
-
-                            uint euro = (uint)((scanCode | (isE0 ? 0xE000 : 0)) << 8);
-
-                            MessageBox.Show(
-                                $"EuroScope-Code: {euro}\nScanCode: {scanCode}\nExtended: {isE0}",
-                                "Key Captured",
-                                MessageBoxButtons.OK,
-                                MessageBoxIcon.Information
-                            );
-                        }
-                    }
-                }
-                finally
-                {
-                    Marshal.FreeHGlobal(buffer);
-                }
-            }
-
-            base.WndProc(ref m);
-        }
-
-        // ------------------------------------------------------------
-        // Strukturen & Konstanten für Raw Input
-        // ------------------------------------------------------------
-        private const int RID_INPUT = 0x10000003;
-        private const int RIM_TYPEKEYBOARD = 1;
-        private const int RI_KEY_E0 = 0x02;
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct RAWINPUTDEVICE
-        {
-            public ushort usUsagePage;
-            public ushort usUsage;
-            public uint dwFlags;
-            public IntPtr hwndTarget;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct RAWINPUTHEADER
-        {
-            public uint dwType;
-            public uint dwSize;
-            public IntPtr hDevice;
-            public IntPtr wParam;
-        }
-
-        [StructLayout(LayoutKind.Explicit)]
-        private struct RAWINPUT
-        {
-            [FieldOffset(0)] public RAWINPUTHEADER header;
-            [FieldOffset(16)] public RAWKEYBOARD data;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct RAWKEYBOARD
-        {
-            public ushort MakeCode;
-            public ushort Flags;
-            public ushort Reserved;
-            public ushort VKey;
-            public uint Message;
-            public uint ExtraInformation;
-        }
-
-        [DllImport("User32.dll")]
-        private static extern bool RegisterRawInputDevices(RAWINPUTDEVICE[] pRawInputDevices, uint uiNumDevices, uint cbSize);
-
-        [DllImport("User32.dll")]
-        private static extern uint GetRawInputData(IntPtr hRawInput, uint uiCommand, IntPtr pData, ref uint pcbSize, uint cbSizeHeader);
     }
 }
