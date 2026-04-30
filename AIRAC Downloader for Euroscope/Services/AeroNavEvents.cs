260d3a81821ccc6e8ad55001e05b6a4124699693
Fabian Mark <fabianmark61@gmail.com>
2026-04-30 23:25:02 +0200

    Added Events triggered by AeroNav

---
+++ AIRAC Downloader for Euroscope/Services/AeroNavEvents.cs
@@ -0,0 +1,14 @@
+﻿namespace AIRAC_Downloader_for_Euroscope.Services
+{
+    public class ToggleDownloadButtonArgs : EventArgs
+    {
+        public bool Enabled { get; }
+        public string PackageName { get; }
+
+        public ToggleDownloadButtonArgs(bool enabled,  string packageName)
+        {
+            Enabled = enabled;
+            PackageName = packageName;
+        }
+    }
+}
