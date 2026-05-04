e70a801b10f1e2c5b6327362aa79932bacd36004
Fabian <fabianmark61@gmail.com>
2026-05-04 11:47:46 +0200

    Implemented G2A and G2G Button Click Event

--- AIRAC Downloader for Euroscope/VccsSettingsControl.xaml.cs
+++ AIRAC Downloader for Euroscope/VccsSettingsControl.xaml.cs
@@ -25,6 +25,9 @@ namespace AIRAC_Downloader_for_Euroscope
             "Windows Audio Session"
         };
 
+        public KeyboardListener.KeyResult? G2aPttKey { get; set; } = null;
+        public KeyboardListener.KeyResult? G2gPttKey { get; set; } = null;
+
         public VccsSettingsControl()
         {
             InitializeComponent();
@@ -45,6 +48,32 @@ namespace AIRAC_Downloader_for_Euroscope
             {
                 this.OutputAudioDevices.Add(dev.FriendlyName);
             }
+        }
+
+        private async void G2A_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            G2A.Content = "Press a key for G2A PTT...";
+            KeyboardListener.KeyResult? newKey = await KeyboardListener.Instance.ListenAsync(this);
+            if (newKey != null)
+                G2aPttKey = newKey;
+
+            if(G2aPttKey != null)
+                G2A.Content = $"G2A PTT: {G2aPttKey.Value.Name}";
+            else
+                G2A.Content = "G2A PTT: Not set";
+        }
+
+        private async void G2G_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            G2G.Content = "Press a key for G2G PTT...";
+            KeyboardListener.KeyResult? newKey = await KeyboardListener.Instance.ListenAsync(this);
+            if (newKey != null)
+                G2gPttKey = newKey;
+
+            if (G2gPttKey != null)
+                G2G.Content = $"G2G PTT: {G2gPttKey.Value.Name}";
+            else
+                G2G.Content = "G2G PTT: Not set";
         }
     }
 }
