2db9d642c3431e3c5553217f79e3404e82bbf545
Fabian Mark <fabianmark61@gmail.com>
2026-05-06 14:10:50 +0200

    SoundEntry and PluginEntry extends INotifyPropertyChanged
    Implemented Logic for Select all and Enable all for Plugins and Sounds
    
    Implemented Logic for single Select and Enable for Plugins and Sounds
    
    Implemented Remove selected Row(s) logic for Plugins and Sounds

--- AIRAC Downloader for Euroscope/EuroscopeSettingsControl.xaml.cs
+++ AIRAC Downloader for Euroscope/EuroscopeSettingsControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Text;
 using System.Windows;
@@ -43,22 +44,83 @@ namespace AIRAC_Downloader_for_Euroscope
             "Pointout",
             "Startup"
         };
-        public class SoundEntry
+        public class SoundEntry : INotifyPropertyChanged
         {
-            public bool IsSelected { get; set; }
-            public bool IsEnabled { get; set; }
+            private bool _isSelected;
+            public bool IsSelected
+            {
+                get => _isSelected;
+                set
+                {
+                    if (_isSelected != value)
+                    {
+                        _isSelected = value;
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsSelected)));
+                    }
+                }
+            }
+
+            private bool _isEnabled;
+            public bool IsEnabled
+            {
+                get => _isEnabled;
+                set
+                {
+                    if (_isEnabled != value)
+                    {
+                        _isEnabled = value;
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsEnabled)));
+                    }
+                }
+            }
+
+
             public string SoundFile { get; set; }
             public string SoundType { get; set; }
+
+
+            public event PropertyChangedEventHandler PropertyChanged;
         }
 
         public ObservableCollection<SoundEntry> Sounds { get; set; } = new();
 
         //Custom Plugins
-        public class PluginEntry
+        public class PluginEntry : INotifyPropertyChanged
         {
-            public bool IsSelected { get; set; }
-            public bool IsEnabled { get; set; }
+            private bool _isSelected;
+            public bool IsSelected
+            {
+                get => _isSelected;
+                set
+                {
+                    if (_isSelected != value)
+                    {
+                        _isSelected = value;
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsSelected)));
+                    }
+                }
+            }
+
+
+            private bool _isEnabled;
+            public bool IsEnabled
+            {
+                get => _isEnabled;
+                set
+                {
+                    if (_isEnabled != value)
+                    {
+                        _isEnabled = value;
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsEnabled)));
+                    }
+                }
+            }
+
+
             public string PluginPath { get; set; }
+
+
+            public event PropertyChangedEventHandler PropertyChanged;
         }
 
         public ObservableCollection<PluginEntry> Plugins { get; set; } = new();
@@ -148,7 +210,6 @@ namespace AIRAC_Downloader_for_Euroscope
             //The named Grid, and not TabControl
             var children = LogicalTreeHelper.GetChildren(MainGrid).OfType<FrameworkElement>().Where(e => e.Name != "CheckEnableAll");
             CheckEnableAll.IsChecked = IsAllChecked(children);
-
         }
         
         private void CheckCallsign_Toggled(object sender, RoutedEventArgs e)
@@ -196,6 +257,276 @@ namespace AIRAC_Downloader_for_Euroscope
         private void CheckEnableAll_Checked(object sender, RoutedEventArgs e)
         {
             CheckCallsign.IsChecked = CheckRealName.IsChecked = CheckCertificate.IsChecked = CheckPassword.IsChecked = CheckFacility.IsChecked = CheckRating.IsChecked = CheckHoppie.IsChecked = CheckEnableAll.IsChecked == true;
+        }
+
+
+
+
+        // -------------------------------
+        // Soundslist
+        // -------------------------------
+
+        // -------------
+        // Select Column
+        // -------------
+        // Update Select All Header if Datagrid checkbox changes
+        private void Sound_UpdateSelectAllHeader()
+        {
+            int IsSelectedCount = 0;
+            foreach (SoundEntry sound in Sounds)
+            {
+                if (sound.IsSelected)
+                {
+                    IsSelectedCount++;
+                }
+            }
+            if (IsSelectedCount == 0)
+            {
+                CheckSelectAllSounds.IsChecked = false;
+            }
+            else if (IsSelectedCount == Sounds.Count)
+            {
+                CheckSelectAllSounds.IsChecked = true;
+            }
+            else
+            {
+                CheckSelectAllSounds.IsChecked = null;
+            }
+        }
+
+        // If Datagrid Checkbox changes update corresponding SoundEntry and call Header Updater
+        private void SoundsList_CurrentCellIsSelectedChanged(object sender, EventArgs e)
+        {
+            var cb = sender as CheckBox;
+            if (cb == null)
+                return;
+
+            var entry = cb.DataContext as SoundEntry;
+            if (entry == null)
+                return;
+
+            // neuen Wert direkt übernehmen
+            entry.IsSelected = cb.IsChecked == true;
+
+            // jetzt Header aktualisieren
+            Sound_UpdateSelectAllHeader();
+        }
+
+        //Update Cell Checkboxes when Header Checkbox clicked
+        private void CheckSelectAllSounds_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is not EuroscopeSettingsControl vm)
+                return;
+
+            bool newValue = ((CheckBox)sender).IsChecked == true;
+
+            foreach (var sound in vm.Sounds)
+                sound.IsSelected = newValue;
+        }
+
+
+        // -------------
+        // Enable Column
+        // -------------
+
+        private void Sound_UpdateEnableAllHeader()
+        {
+            int IsEnabledCount = 0;
+            foreach (SoundEntry sound in Sounds)
+            {
+                if (sound.IsEnabled)
+                {
+                    IsEnabledCount++;
+                }
+            }
+            if (IsEnabledCount == 0)
+            {
+                CheckEnableAllSounds.IsChecked = false;
+            }
+            else if (IsEnabledCount == Sounds.Count)
+            {
+                CheckEnableAllSounds.IsChecked = true;
+            }
+            else
+            {
+                CheckEnableAllSounds.IsChecked = null;
+            }
+        }
+
+        // If Datagrid Checkbox changes update corresponding SoundEntry and call Header Updater
+        private void SoundsList_CurrentCellIsEnabledChanged(object sender, EventArgs e)
+        {
+            var cb = sender as CheckBox;
+            if (cb == null)
+                return;
+
+            var entry = cb.DataContext as SoundEntry;
+            if (entry == null)
+                return;
+
+            // neuen Wert direkt übernehmen
+            entry.IsEnabled = cb.IsChecked == true;
+
+            // jetzt Header aktualisieren
+            Sound_UpdateEnableAllHeader();
+        }
+
+        //Update Cell Checkboxes when Header Checkbox clicked
+        private void CheckEnableAllSounds_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is not EuroscopeSettingsControl vm)
+                return;
+
+            bool newValue = ((CheckBox)sender).IsChecked == true;
+
+            foreach (var sound in vm.Sounds)
+                sound.IsEnabled = newValue;
+        }
+
+        private void Click_RemoveSoundRow(object sender, RoutedEventArgs e)
+        {
+            var toRemove = Sounds.Where(s => s.IsSelected).ToList();
+            foreach(var sound in toRemove)
+                Sounds.Remove(sound);
+
+            Sound_UpdateEnableAllHeader();
+            Sound_UpdateSelectAllHeader();
+        }
+
+
+
+
+
+
+        // -------------------------------
+        // Pluginslist
+        // -------------------------------
+
+        // -------------
+        // Select Column
+        // -------------
+        // Update Select All Header if Datagrid checkbox changes
+        private void Plugin_UpdateSelectAllHeader()
+        {
+            int IsSelectedCount = 0;
+            foreach (PluginEntry Plugin in Plugins)
+            {
+                if (Plugin.IsSelected)
+                {
+                    IsSelectedCount++;
+                }
+            }
+            if (IsSelectedCount == 0)
+            {
+                CheckSelectAllPlugins.IsChecked = false;
+            }
+            else if (IsSelectedCount == Plugins.Count)
+            {
+                CheckSelectAllPlugins.IsChecked = true;
+            }
+            else
+            {
+                CheckSelectAllPlugins.IsChecked = null;
+            }
+        }
+
+        // If Datagrid Checkbox changes update corresponding PluginEntry and call Header Updater
+        private void PluginsList_CurrentCellIsSelectedChanged(object sender, EventArgs e)
+        {
+            var cb = sender as CheckBox;
+            if (cb == null)
+                return;
+
+            var entry = cb.DataContext as PluginEntry;
+            if (entry == null)
+                return;
+
+            // neuen Wert direkt übernehmen
+            entry.IsSelected = cb.IsChecked == true;
+
+            // jetzt Header aktualisieren
+            Plugin_UpdateSelectAllHeader();
+        }
+
+        //Update Cell Checkboxes when Header Checkbox clicked
+        private void CheckSelectAllPlugins_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is not EuroscopeSettingsControl vm)
+                return;
+
+            bool newValue = ((CheckBox)sender).IsChecked == true;
+
+            foreach (var Plugin in vm.Plugins)
+                Plugin.IsSelected = newValue;
+        }
+
+
+        // -------------
+        // Enable Column
+        // -------------
+
+        private void Plugin_UpdateEnableAllHeader()
+        {
+            int IsEnabledCount = 0;
+            foreach (PluginEntry Plugin in Plugins)
+            {
+                if (Plugin.IsEnabled)
+                {
+                    IsEnabledCount++;
+                }
+            }
+            if (IsEnabledCount == 0)
+            {
+                CheckEnableAllPlugins.IsChecked = false;
+            }
+            else if (IsEnabledCount == Plugins.Count)
+            {
+                CheckEnableAllPlugins.IsChecked = true;
+            }
+            else
+            {
+                CheckEnableAllPlugins.IsChecked = null;
+            }
+        }
+
+        // If Datagrid Checkbox changes update corresponding PluginEntry and call Header Updater
+        private void PluginsList_CurrentCellIsEnabledChanged(object sender, EventArgs e)
+        {
+            var cb = sender as CheckBox;
+            if (cb == null)
+                return;
+
+            var entry = cb.DataContext as PluginEntry;
+            if (entry == null)
+                return;
+
+            // neuen Wert direkt übernehmen
+            entry.IsEnabled = cb.IsChecked == true;
+
+            // jetzt Header aktualisieren
+            Plugin_UpdateEnableAllHeader();
+        }
+
+        //Update Cell Checkboxes when Header Checkbox clicked
+        private void CheckEnableAllPlugins_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is not EuroscopeSettingsControl vm)
+                return;
+
+            bool newValue = ((CheckBox)sender).IsChecked == true;
+
+            foreach (var Plugin in vm.Plugins)
+                Plugin.IsEnabled = newValue;
+        }
+
+        private void Click_RemovePluginRow(object sender, RoutedEventArgs e)
+        {
+            var toRemove = Plugins.Where(s => s.IsSelected).ToList();
+            foreach (var Plugin in toRemove)
+                Plugins.Remove(Plugin);
+
+            Plugin_UpdateEnableAllHeader();
+            Plugin_UpdateSelectAllHeader();
         }
     }
 }
